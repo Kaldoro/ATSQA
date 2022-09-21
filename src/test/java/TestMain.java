78a1ad5169f743907c2b15ec8b5517391b1ee995
Danny van Veltum <d.j.vanveltum@students.uu.nl>
2022-09-21 16:50:24 +0200

    Fixed magic numbers in test

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -6,84 +6,172 @@ import java.util.HashMap;
 
 class TestMain {
 
-    String[] exampleTestSmellResult = new String[] {
-            "App;TestFileName;ProductionFileName;LOC;numberMethods;Assertion Roulette;Eager Test;Mystery Guest;Sleepy Test;Unknown Test;Redundant Assertion;Dependent Test;Magic Number Test;Conditional Test Logic;EmptyTest;General Fixture;IgnoredTest;Sensitive Equality;Verbose Test;Default Test;Resource Optimism;Duplicate Assert;Exception Catching Throwing;Constructor Initialization;Print Statement;Lazy Test",
-            "testProject;testFile1;file1;50;16;8;0;0;0;0;0;8;0;0;0;0;0;0;0;0;0;0;0;0;0;5",
-            "testProject;testFile2;file2;40;14;5;1;1;1;1;1;0;1;1;1;1;1;1;1;1;1;1;1;1;1;0"
-    };
+    static class TestFile {
+        public String fileName;
+        public String productionFileName;
+        public int LOC;
+        public int numberMethods;
+        public int assertionRoulette;
+        public int eagerTest;
+        public int mysteryGuest;
+        public int sleepyTest;
+        public int unknownTest;
+        public int redundantAssertion;
+        public int dependentTest;
+        public int magicNumberTest;
+        public int conditionalTestLogic;
+        public int emptyTest;
+        public int generalFixture;
+        public int ignoredTest;
+        public int sensitiveEquality;
+        public int verboseTest;
+        public int defaultTest;
+        public int resourceOptimism;
+        public int duplicateAssert;
+        public int exceptionCatchingThrowing;
+        public int constructorInitialization;
+        public int printStatement;
+        public int lazyTest;
+        public TestFile(String file_name, String production_file_name, int TLOC, int number_methods, int assertion_roulette, int eager_test, int mystery_guest, int sleepy_test,
+                        int unknown_test, int redundant_assertion, int dependent_test, int magic_number_test, int conditional_test_logic, int empty_test, int general_fixture,
+                        int ignored_test, int sensitive_equality, int verbose_test, int default_test, int resource_optimism, int duplicate_assert, int exception_catching_throwing,
+                        int constructor_initialization, int print_statement, int lazy_test) {
+            fileName = file_name;
+            productionFileName = production_file_name;
+            LOC = TLOC;
+            numberMethods = number_methods;
+            assertionRoulette = assertion_roulette;
+            eagerTest = eager_test;
+            mysteryGuest = mystery_guest;
+            sleepyTest = sleepy_test;
+            unknownTest = unknown_test;
+            redundantAssertion = redundant_assertion;
+            dependentTest = dependent_test;
+            magicNumberTest = magic_number_test;
+            conditionalTestLogic = conditional_test_logic;
+            emptyTest = empty_test;
+            generalFixture = general_fixture;
+            ignoredTest = ignored_test;
+            sensitiveEquality = sensitive_equality;
+            verboseTest = verbose_test;
+            defaultTest = default_test;
+            resourceOptimism = resource_optimism;
+            duplicateAssert = duplicate_assert;
+            exceptionCatchingThrowing = exception_catching_throwing;
+            constructorInitialization = constructor_initialization;
+            printStatement = print_statement;
+            lazyTest = lazy_test;
+        }
+        public String printFileInfo () {
+            String testProject = "testProject";
+            return testProject + ";" + fileName + ";" + productionFileName + ";" + LOC + ";" + numberMethods + ";" + assertionRoulette + ";" + eagerTest + ";" + mysteryGuest + ";" + sleepyTest + ";" + unknownTest + ";" + redundantAssertion + ";" + dependentTest + ";" + magicNumberTest + ";" + conditionalTestLogic + ";" + emptyTest + ";" + generalFixture + ";" + ignoredTest + ";" + sensitiveEquality + ";" + verboseTest + ";" + defaultTest + ";" + resourceOptimism + ";" + duplicateAssert + ";" + exceptionCatchingThrowing + ";" + constructorInitialization + ";" + printStatement + ";" + lazyTest ;
+        }
+
+    }
+    TestFile testFile1 = new TestFile("testFile1", "file1", 50, 16, 8, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5);
+    TestFile testFile2 = new TestFile("testFile2", "file2", 40, 14, 5, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0);
+
     @Test
     void testGetSmells() {
+        String[] exampleTestSmellResult = new String[] {
+                "App;TestFileName;ProductionFileName;LOC;numberMethods;Assertion Roulette;Eager Test;Mystery Guest;Sleepy Test;Unknown Test;Redundant Assertion;Dependent Test;Magic Number Test;Conditional Test Logic;EmptyTest;General Fixture;IgnoredTest;Sensitive Equality;Verbose Test;Default Test;Resource Optimism;Duplicate Assert;Exception Catching Throwing;Constructor Initialization;Print Statement;Lazy Test",
+                testFile1.printFileInfo(),
+                testFile2.printFileInfo()
+        };
         HashMap<String,Integer> result = main.getSmells(exampleTestSmellResult);
         HashMap<String,Integer> expectedOutput = new HashMap<>();
 
-        expectedOutput.put("LOC", 90);
-        assertEquals(90,result.get("LOC"));
+        int expectedLOC = testFile1.LOC + testFile2.LOC;
+        expectedOutput.put("LOC", expectedLOC);
+        assertEquals(expectedLOC,result.get("LOC"));
 
-        expectedOutput.put("numberMethods", 30);
-        assertEquals(30,result.get("numberMethods"));
+        int expectedNumberMethods = testFile1.numberMethods + testFile2.numberMethods;
+        expectedOutput.put("numberMethods", expectedNumberMethods);
+        assertEquals(expectedNumberMethods,result.get("numberMethods"));
 
-        expectedOutput.put("Assertion Roulette",13);
-        assertEquals(13,result.get("Assertion Roulette"));
+        int expectedAssertionRoulette = testFile1.assertionRoulette + testFile2.assertionRoulette;
+        expectedOutput.put("Assertion Roulette",expectedAssertionRoulette);
+        assertEquals(expectedAssertionRoulette,result.get("Assertion Roulette"));
 
-        expectedOutput.put("Eager Test", 1);
-        assertEquals(1,result.get("Eager Test"));
+        int expectedEagerTest = testFile1.eagerTest + testFile2.eagerTest;
+        expectedOutput.put("Eager Test", expectedEagerTest);
+        assertEquals(expectedEagerTest,result.get("Eager Test"));
 
-        expectedOutput.put("Mystery Guest", 1);
-        assertEquals(1,result.get("Mystery Guest"));
+        int expectedMysteryGuest = testFile1.mysteryGuest + testFile2.mysteryGuest;
+        expectedOutput.put("Mystery Guest", expectedMysteryGuest);
+        assertEquals(expectedMysteryGuest,result.get("Mystery Guest"));
 
-        expectedOutput.put("Sleepy Test", 1);
-        assertEquals(1,result.get("Sleepy Test"));
+        int expectedSleepyTest = testFile1.sleepyTest + testFile2.sleepyTest;
+        expectedOutput.put("Sleepy Test", expectedSleepyTest);
+        assertEquals(expectedSleepyTest,result.get("Sleepy Test"));
 
-        expectedOutput.put("Unknown Test", 1);
-        assertEquals(1,result.get("Unknown Test"));
+        int expectedUnknownTest = testFile1.unknownTest + testFile2.unknownTest;
+        expectedOutput.put("Unknown Test", expectedUnknownTest);
+        assertEquals(expectedUnknownTest,result.get("Unknown Test"));
 
-        expectedOutput.put("Redundant Assertion", 1);
-        assertEquals(1,result.get("Redundant Assertion"));
+        int expectedRedundantAssertion = testFile1.redundantAssertion + testFile2.redundantAssertion;
+        expectedOutput.put("Redundant Assertion", expectedRedundantAssertion);
+        assertEquals(expectedRedundantAssertion,result.get("Redundant Assertion"));
 
-        expectedOutput.put("Dependent Test", 8);
-        assertEquals(8,result.get("Dependent Test"));
+        int expectedDependentTest = testFile1.dependentTest + testFile2.dependentTest;
+        expectedOutput.put("Dependent Test", expectedDependentTest);
+        assertEquals(expectedDependentTest,result.get("Dependent Test"));
 
-        expectedOutput.put("Magic Number Test", 1);
-        assertEquals(1,result.get("Magic Number Test"));
+        int expectedMagicNumberTest = testFile1.magicNumberTest + testFile2.magicNumberTest;
+        expectedOutput.put("Magic Number Test", expectedMagicNumberTest);
+        assertEquals(expectedMagicNumberTest,result.get("Magic Number Test"));
 
-        expectedOutput.put("Resource Optimism", 1);
-        assertEquals(1,result.get("Resource Optimism"));
+        int expectedResourceOptimism = testFile1.resourceOptimism + testFile2.resourceOptimism;
+        expectedOutput.put("Resource Optimism", expectedResourceOptimism);
+        assertEquals(expectedResourceOptimism,result.get("Resource Optimism"));
 
-        expectedOutput.put("Conditional Test Logic", 1);
-        assertEquals(1,result.get("Conditional Test Logic"));
+        int expectedConditionalTestLogic = testFile1.conditionalTestLogic + testFile2.conditionalTestLogic;
+        expectedOutput.put("Conditional Test Logic", expectedConditionalTestLogic);
+        assertEquals(expectedConditionalTestLogic,result.get("Conditional Test Logic"));
 
-        expectedOutput.put("EmptyTest", 1);
-        assertEquals(1,result.get("EmptyTest"));
+        int expectedEmptyTest = testFile1.emptyTest + testFile2.emptyTest;
+        expectedOutput.put("EmptyTest", expectedEmptyTest);
+        assertEquals(expectedEmptyTest,result.get("EmptyTest"));
 
-        expectedOutput.put("General Fixture", 1);
-        assertEquals(1,result.get("General Fixture"));
+        int expectedGeneralFixture = testFile1.generalFixture + testFile2.generalFixture;
+        expectedOutput.put("General Fixture", expectedGeneralFixture);
+        assertEquals(expectedGeneralFixture,result.get("General Fixture"));
 
-        expectedOutput.put("IgnoredTest", 1);
-        assertEquals(1,result.get("IgnoredTest"));
+        int expectedIgnoredTest = testFile1.ignoredTest + testFile2.ignoredTest;
+        expectedOutput.put("IgnoredTest", expectedIgnoredTest);
+        assertEquals(expectedIgnoredTest,result.get("IgnoredTest"));
 
-        expectedOutput.put("Sensitive Equality", 1);
-        assertEquals(1,result.get("Sensitive Equality"));
+        int expectedSensitiveEquality = testFile1.sensitiveEquality + testFile2.sensitiveEquality;
+        expectedOutput.put("Sensitive Equality", expectedSensitiveEquality);
+        assertEquals(expectedSensitiveEquality,result.get("Sensitive Equality"));
 
-        expectedOutput.put("Verbose Test", 1);
-        assertEquals(1,result.get("Verbose Test"));
+        int expectedVerboseTest = testFile1.verboseTest + testFile2.verboseTest;
+        expectedOutput.put("Verbose Test", expectedVerboseTest);
+        assertEquals(expectedVerboseTest,result.get("Verbose Test"));
 
-        expectedOutput.put("Default Test", 1);
-        assertEquals(1,result.get("Default Test"));
+        int expectedDefaultTest = testFile1.defaultTest + testFile2.defaultTest;
+        expectedOutput.put("Default Test", expectedDefaultTest);
+        assertEquals(expectedDefaultTest,result.get("Default Test"));
 
-        expectedOutput.put("Duplicate Assert", 1);
-        assertEquals(1,result.get("Duplicate Assert"));
+        int expectedDuplicateAssert = testFile1.duplicateAssert + testFile2.duplicateAssert;
+        expectedOutput.put("Duplicate Assert", expectedDuplicateAssert);
+        assertEquals(expectedDuplicateAssert,result.get("Duplicate Assert"));
 
-        expectedOutput.put("Exception Catching Throwing", 1);
-        assertEquals(1,result.get("Exception Catching Throwing"));
+        int expectedExceptionCatchingThrowing = testFile1.exceptionCatchingThrowing + testFile2.exceptionCatchingThrowing;
+        expectedOutput.put("Exception Catching Throwing", expectedExceptionCatchingThrowing);
+        assertEquals(expectedExceptionCatchingThrowing,result.get("Exception Catching Throwing"));
 
-        expectedOutput.put("Constructor Initialization", 1);
-        assertEquals(1,result.get("Constructor Initialization"));
+        int expectedConstructorInitialization = testFile1.constructorInitialization + testFile2.constructorInitialization;
+        expectedOutput.put("Constructor Initialization", expectedConstructorInitialization);
+        assertEquals(expectedConstructorInitialization,result.get("Constructor Initialization"));
 
-        expectedOutput.put("Print Statement", 1);
-        assertEquals(1,result.get("Print Statement"));
+        int expectedPrintStatement = testFile1.printStatement + testFile2.printStatement;
+        expectedOutput.put("Print Statement", expectedPrintStatement);
+        assertEquals(expectedPrintStatement,result.get("Print Statement"));
 
-        expectedOutput.put("Lazy Test", 5);
-        assertEquals(5,result.get("Lazy Test"));
+        int expectedLazyTest = testFile1.lazyTest + testFile2.lazyTest;
+        expectedOutput.put("Lazy Test", expectedLazyTest);
+        assertEquals(expectedLazyTest,result.get("Lazy Test"));
 
         //Check that total result is equal
         assertEquals(expectedOutput, result);
